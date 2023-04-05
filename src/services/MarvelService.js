76b69dbbbc10c31135bbd723d041ca33f0895f09
Yuriy Ovsyannikov <yovsyannikov@gmail.com>
2023-04-05 23:15:59 +0300

    add prop-types

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=81814a9fcb9dfb59ea1e6a75b1230fbc';
+    _baseOffset = 210;
     getResorce = async (url) => {
         let res = await fetch(url);
 
@@ -11,8 +12,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResorce(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResorce(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
