c9f2fab7837e4413f23a3c92c4d9b86ae4889734
Yuriy Ovsyannikov <yovsyannikov@gmail.com>
2023-04-15 23:52:28 +0300

    Add comics + hooks

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,45 +1,26 @@
-import { Component } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import { MainPage, ComicsPage, Page404, SingleComicPage } from "../pages";
 
-import decoration from '../../resources/img/vision.png';
 
-class App extends Component {
+const App = () => {
 
-    state = {
-        selectedChar: null
-    }
 
-    onCharSelected = (id) => {
-        this.setState({
-            selectedChar: id
-        })
-    }
-
-    render () {
         return (
-            <div className="app">
-                <AppHeader/>
-                <main>
-                    <ErrorBoundary>
-                        <RandomChar/>
-                    </ErrorBoundary>
-                    <div className="char__content">
-                        <ErrorBoundary>
-                        <CharList onCharSelected={this.onCharSelected}/>
-                        </ErrorBoundary>
-                        <ErrorBoundary>
-                        <CharInfo charId={this.state.selectedChar}/>
-                        </ErrorBoundary>
-                    </div>
-                    <img className="bg-decoration" src={decoration} alt="vision"/>
-                </main>
-            </div>
+            <Router>
+                <div className="app">
+                    <AppHeader/>
+                    <main>
+                        <Routes>
+                            <Route path='/' element={<MainPage/>}/>
+                            <Route path='/comics' element={<ComicsPage/>}/>
+                            <Route path='*' element={<Page404/>}/>
+                            <Route path='/comics/:comicId' element={<SingleComicPage/>}/>
+                        </Routes>
+                    </main>
+                </div>
+            </Router>
         )
     }
-}
 
 export default App;
--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,95 +1,62 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-import MarvelService from '../../services/MarvelService';
+import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
-import Skeleton from '../skeleton/Skeleton'
+import Skeleton from '../skeleton/Skeleton';
 
 import './charInfo.scss';
 
-class CharInfo extends Component {
+const CharInfo = (props) => {
 
-    state = {
-        char: null,
-        loading: false,
-        error:false
-    }
+    const [char, setChar] = useState(null);
 
-    marvelService = new MarvelService();
+    const {loading, error, getCharacter, clearError} = useMarvelService();
 
-    componentDidMount() {
-        this.updateChar();
-    }
+    useEffect(() => {
+        updateChar()
+    }, [props.charId])
 
-    componentDidUpdate(prevProps){
-        if (this.props.charId !== prevProps.charId) {
-            this.updateChar();
-        }
-    }
-
-    updateChar = () => {
-        const {charId} = this.props;
+    const updateChar = () => {
+        const {charId} = props;
         if (!charId) {
             return;
         }
-
-        this.onCharLoading();
-
-        this.marvelService
-            .getCharacter(charId)
-            .then(this.onCharLoaded)
-            .catch(this.onError);
-        
-        }
-
-        onCharLoaded = (char) => {
-            this.setState({
-                char, 
-                loading: false
-            })
-        }
-    
-        onCharLoading = () => {
-            this.setState({
-                loading: true
-            })
-        }
-    
-        onError = () => {
-            this.setState({
-                loading: false,
-                error: true
-            })
-        }
-
-    render () {
-        const {char, loading, error} = this.state;
-
-        const skeleton = char || loading || error ? null : <Skeleton/>
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
-        const content = !(loading || error || !char) ? <View char = {char}/> : null;
-        return (
-            <div className="char__info">
-                {skeleton}
-                {errorMessage}
-                {spinner}
-                {content}
-            </div>
-        )
+            clearError();
+            getCharacter(charId)
+            .then(onCharLoaded)
     }
+
+    const onCharLoaded = (char) => {
+        setChar(char);
+    }
+
+    const skeleton = char || loading || error ? null : <Skeleton/>;
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error || !char) ? <View char={char}/> : null;
+
+    return (
+        <div className="char__info">
+            {skeleton}
+            {errorMessage}
+            {spinner}
+            {content}
+        </div>
+    )
 }
 
 const View = ({char}) => {
-    const {name, description, thumbnail, homepage, wiki, comics} =  char;
-    
+    const {name, description, thumbnail, homepage, wiki, comics} = char;
+
     let imgStyle = {'objectFit' : 'cover'};
     if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
         imgStyle = {'objectFit' : 'contain'};
     }
+
     return (
-        <> 
+        <>
             <div className="char__basics">
                 <img src={thumbnail} alt={name} style={imgStyle}/>
                 <div>
@@ -120,7 +87,7 @@ const View = ({char}) => {
                             </li>
                         )
                     })
-                }
+                }                
             </ul>
         </>
     )
